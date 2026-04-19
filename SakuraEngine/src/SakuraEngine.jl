e7bec3534f66e91e1ca6762cf4fb4339d777545e
zzztzzzt <180073683+zzztzzzt@users.noreply.github.com>
2026-04-20 01:07:14 +0800

    [feat] add sk-if. update example_template. add test script

--- SakuraEngine/src/SakuraEngine.jl
+++ SakuraEngine/src/SakuraEngine.jl
@@ -22,29 +22,57 @@ function eval_script(script::AbstractString)
 end
 
 function render_template(template::AbstractString, mod::Module)
-    return replace(template, r"\{\{(.*?)\}\}"s => function(m)
-        # Process Substring and remove {{ }}
-        clean_expr = strip(m[3:end-2])
+    template = process_sk_if(template, mod)
+
+    template = replace(template, r"\{\{(.*?)\}\}"s => function(m)
+        # Rematch to ensure it get captures
+        caps = match(r"\{\{(.*?)\}\}"s, m).captures
+        clean_expr = strip(caps[1])
         
-        # Parse the string into an expression
         ex = Meta.parse(clean_expr)
-        
-        # Execute within the specified mod module, and explicitly use Core.eval
         val = Core.eval(mod, ex)
-        
         return string(val)
     end)
+
+    return template
 end
 
 function render_file(path::String)
     content = read(path, String)
-
     script, template = extract_blocks(content)
     mod = eval_script(script)
-
     html = render_template(template, mod)
 
-    return html
+    # Cleanup : Reduce multiple consecutive line breaks to one and remove lines containing only whitespace
+    html = replace(html, r"\n\s*\n" => "\n")
+    return strip(html)
+end
+
+function process_sk_if(template::AbstractString, mod::Module)
+    pattern = r"<(\w+)([^>]*)\s+sk-if=\"(.*?)\"([^>]*)>(.*?)</\1>"s
+
+    return replace(template, pattern => function(m)
+        caps = match(pattern, m).captures
+        tag = caps[1]
+        before_attrs = caps[2]
+        condition_str = caps[3]
+        after_attrs = caps[4]
+        inner = caps[5]
+
+        # eval condition
+        cond_expr = Meta.parse(condition_str)
+        result = Core.eval(mod, cond_expr)
+
+        if result
+            combined_attrs = strip(string(before_attrs, " ", after_attrs))
+            # If the attribute is empty, do not add a space; if there is an attribute, add a space before it
+            attr_str = isempty(combined_attrs) ? "" : " " * combined_attrs
+            
+            return "<$tag$attr_str>$inner</$tag>"
+        else
+            return ""
+        end
+    end)
 end
 
 end # module SakuraEngine
---
+++ scripts/test_sakura_engine.jl
@@ -0,0 +1,4 @@
+using SakuraEngine
+
+html = SakuraEngine.render_file("template_example/example.sk")
+println(html)
